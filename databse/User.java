99d0d9ddca9ed65244cd534a3265d73364c4cffc
Sakesh-A <a.sakesh@gmail.com>
2024-10-31 16:43:02 -0400

    added fields

--- databse/User.java
+++ databse/User.java
@@ -1,3 +1,7 @@
 public class User implements UserInterface{
-
+    private String username;
+    private String password;
+    private ArrayList<User> friends;
+    private ArrayList<User> blockedUsers;
+    private boolean privateOrPublic;
 }
