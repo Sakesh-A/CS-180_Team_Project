abcc1267700e979ac11e48193742525be463c596
Sakesh-A <a.sakesh@gmail.com>
2024-10-31 15:58:58 -0400

    added all of the methods to the UserInterface

--- databse/UserInterface.java
+++ databse/UserInterface.java
@@ -1,1 +1,8 @@
-
+public UserInterface {
+    public void sendMessage();
+    public void receiveMessage();
+    public void deleteMessage();
+    public void addFriend();
+    public void removeFriend();
+    public void blockUser();
+}
