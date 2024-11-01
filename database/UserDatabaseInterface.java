5b66f2dd2ad26850df98f23772ce5dee6999af68
Daniel Zhang <zhan4728@purdue.edu>
2024-11-01 17:03:53 -0400

    Made changes

--- database/UserDatabaseInterface.java
+++ database/UserDatabaseInterface.java
@@ -4,6 +4,6 @@ public interface UserDatabaseInterface {
     ArrayList<User> getUsers();
     void setUsers(ArrayList<User> users);
     void addUser(User user);
-    //boolean saveUser(String username, String password, boolean allowMessagesFromAnyone);
+    boolean saveUser(String username, String password, boolean allowMessagesFromAnyone);
     boolean validateLogin(String username, String password);
 }
