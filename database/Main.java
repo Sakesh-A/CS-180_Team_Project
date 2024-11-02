23c5fb746d4201776e372a1fc087fd687205f5f5
Sakesh-A <a.sakesh@gmail.com>
2024-11-02 19:37:39 -0400

    changed UserDatabaseInterface

--- database/Main.java
+++ database/Main.java
@@ -1,46 +1,46 @@
-import java.util.*;
-import java.io.*;
-
-
-public class Main {
-    public static void main(String[] args) {
-        UserDatabase db = new UserDatabase();
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("enter your username:");
-        String user = sc.nextLine();
-
-        System.out.println("Enter your password:");
-        String pass = sc.nextLine();
-
-        while (!db.isPasswordValid(pass)) {
-            System.out.println("invalid, try again");
-            pass = sc.nextLine();
-        }
-
-        System.out.println("would you like to recieve messages from anybody?");
-        String choice = sc.nextLine();
-
-        boolean yOrN = false;
-
-        if (choice.equals("y")) {
-            yOrN = true;
-        } else {
-            yOrN = false;
-        }
-
-        if(db.saveUser(user, pass, yOrN)) {
-            String filePath = "UsersList.txt";
-            try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-                String line;
-                while ((line = br.readLine()) != null) {
-                    System.out.println(line);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-
-    }
-}
+//import java.util.*;
+//import java.io.*;
+//
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        UserDatabase db = new UserDatabase();
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("enter your username:");
+//        String user = sc.nextLine();
+//
+//        System.out.println("Enter your password:");
+//        String pass = sc.nextLine();
+//
+//        while (!db.isPasswordValid(pass)) {
+//            System.out.println("invalid, try again");
+//            pass = sc.nextLine();
+//        }
+//
+//        System.out.println("would you like to recieve messages from anybody?");
+//        String choice = sc.nextLine();
+//
+//        boolean yOrN = false;
+//
+//        if (choice.equals("y")) {
+//            yOrN = true;
+//        } else {
+//            yOrN = false;
+//        }
+//
+//        if(db.saveUser(user, pass, yOrN)) {
+//            String filePath = "UsersList.txt";
+//            try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+//                String line;
+//                while ((line = br.readLine()) != null) {
+//                    System.out.println(line);
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//
+//
+//    }
+//}
--- database/UserDatabaseInterface.java
+++ database/UserDatabaseInterface.java
@@ -3,7 +3,7 @@ import java.util.*;
 public interface UserDatabaseInterface {
     ArrayList<User> getUsers();
     void setUsers(ArrayList<User> users);
-    void addUser(User user);
-    boolean saveUser(String username, String password, boolean allowMessagesFromAnyone);
-    boolean validateLogin(String username, String password);
+    public boolean addUser(User user);
+    public boolean removeUser(User user);
+    public boolean usersToFile();
 }
