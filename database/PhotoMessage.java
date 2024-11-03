076c81fdccc82562057b6f28670ee8a7f8662ab3
Zoconne <zoconne@purdue.edu>
2024-11-03 17:53:57 -0500

    Added coding standard requirements

--- database/PhotoMessage.java
+++ database/PhotoMessage.java
@@ -1,36 +1,49 @@
-import java.util.*;
-import java.io.*;
+import java.util.*; // Avoid wildcard imports; only import specific classes if needed
+import java.io.*; // Comment or remove unnecessary imports to improve code readability
 
 /**
  * Team Project -- PhotoMessage
  *
- * Creates a PhotoMessage object and includes gets/sets
- *
- *
- * @author Mahith Narreddy, Daniel Zhang, Sakesh Andhavarapu, Zachary O'Connell, Seth Jeevanandham
- *
- * @version Nov 3, 2024
- *
+ * Represents a photo message with additional attributes for sender, receiver, message content, 
+ * and a photo attachment. Extends the TextMessage class to include photo-related functionality.
+ * 
+ * Authors: Mahith Narreddy, Daniel Zhang, Sakesh Andhavarapu, Zachary O'Connell, Seth Jeevanandham
+ * Version: Nov 3, 2024
  */
 
 public class PhotoMessage extends TextMessage {
 
-
-
+    // Stores the file path or URL to the photo attached to the message
     private String photo;
 
-
+    /**
+     * Constructs a PhotoMessage object with message content, sender, receiver, and a photo.
+     *
+     * @param message the text content of the message
+     * @param sender the User object representing the sender
+     * @param receiver the User object representing the receiver
+     * @param photo the file path or URL to the photo attachment as a String
+     */
     public PhotoMessage(String message, User sender, User receiver, String photo) {
         super(message, sender, receiver);
         this.photo = photo;
     }
 
-
+    /**
+     * Gets the photo attachment associated with this message.
+     *
+     * @return the file path or URL of the photo as a String
+     */
     public String getPhoto() {
         return photo;
     }
+
+    /**
+     * Sets a new photo attachment for this message.
+     *
+     * @param photo the file path or URL of the new photo as a String
+     */
     public void setPhoto(String photo) {
         this.photo = photo;
     }
-
 }
--- database/TextMessage.java
+++ database/TextMessage.java
@@ -1,85 +1,98 @@
-import java.util.*;
+import java.util.*; 
 import java.io.*;
 
 /**
  * Team Project -- TextMessage
  *
- * Creates a TextMessage object and includes all needed methods including gets and sets
- *
- *
- * @author Mahith Narreddy, Daniel Zhang, Sakesh Andhavarapu, Zachary O'Connell, Seth Jeevanandham
- *
- * @version Nov 3, 2024
- *
+ * Represents a text message object with basic properties of sender, receiver, and message content.
+ * This class implements the MessageInterface and provides methods for accessing message details.
+ * 
+ * Authors: Mahith Narreddy, Daniel Zhang, Sakesh Andhavarapu, Zachary O'Connell, Seth Jeevanandham
+ * Version: Nov 3, 2024
  */
 
-public class TextMessage implements MessageInterface{
+public class TextMessage implements MessageInterface {
+   
+   // Stores sender, receiver usernames, and message content in a String array of length 3
    private String[] messageArray = new String[3];
 
-//    private String message;
-//    private User sender;
-//    private User receiver;
-//    private int messageId;
-//    public static int id = 0;
+   // Old code with direct attributes has been commented out to simplify access through the array
+   // Uncomment if additional functionality or separate variables are needed in future
+   
+   /**
+    * Constructor for TextMessage. Initializes the message array with sender username, receiver username, 
+    * and message content.
+    *
+    * @param message the message content
+    * @param sender the User object representing the message sender
+    * @param receiver the User object representing the message receiver
+    */
+   public TextMessage(String message, User sender, User receiver) {
+       messageArray[0] = sender.getUsername();
+       messageArray[1] = receiver.getUsername();
+       messageArray[2] = message;
+   }
 
-    public TextMessage(String message, User sender, User receiver) {
-        messageArray[0] = sender.getUsername();
-        messageArray[1] = receiver.getUsername();
-        messageArray[2] = message;
+   /**
+    * Returns the username of the sender.
+    *
+    * @return sender's username as a String
+    */
+   public String getSenderUsername() {
+       return messageArray[0];
+   }
 
-        //        this.message = message;
-//        this.sender = sender;
-//        this.receiver = receiver;
-//        messageId = id;
-    }
-//    public int getMessageId() {
-//        return messageId;
-//    }
+   /**
+    * Returns the username of the receiver.
+    *
+    * @return receiver's username as a String
+    */
+   public String getReceiverUsername() {
+       return messageArray[1];
+   }
 
-//   public  String getMessage() {
-//        return message;
-//    }
-//
-//    public void setMessage(String message) {
-//        this.message = message;
-//    }
-//
-//    public User getSender() {
-//        return sender;
-//    }
-//
-//    public User getReceiver() {
-//        return receiver;
-//    }
-//
-//    public void setSender(User sender) {
-//        this.sender = sender;
-//    }
-//
-//    public void setReceiver(User receiver) {
-//        this.receiver = receiver;
-//    }
+   /**
+    * Returns the content of the message.
+    *
+    * @return message content as a String
+    */
+   public String getMessageContent() {
+       return messageArray[2];
+   }
 
-    public String getSenderUsername() { return messageArray[0]; }
-    public String getReceiverUsername() { return messageArray[1]; }
-    public String getMessageContent() { return messageArray[2]; }
+   /**
+    * Returns a copy of the messageArray to prevent direct modification of the original array.
+    *
+    * @return a cloned String array containing sender username, receiver username, and message content
+    */
+   public String[] getMessageArray() {
+       return messageArray.clone();
+   }
 
-    public String[] getMessageArray() {
-        return messageArray.clone();
-    }
+   /**
+    * Checks equality between this TextMessage instance and another object.
+    * 
+    * @param o the object to compare
+    * @return true if o is a TextMessage with identical sender, receiver, and message; false otherwise
+    */
+   @Override
+   public boolean equals(Object o) {
+       if (!(o instanceof TextMessage m)) {
+           return false;
+       }
 
+       return (this.messageArray[0].equals(m.messageArray[0]) &&
+               this.messageArray[1].equals(m.messageArray[1]) &&
+               this.messageArray[2].equals(m.messageArray[2]));
+   }
 
-    public boolean equals(Object o) {
-        if(!(o instanceof TextMessage m)) {
-            return false;
-        }
-
-        return (this.messageArray[0].equals(m.messageArray[0]) && this.messageArray[1].equals(m.messageArray[1])
-       && this.messageArray[2].equals(m.messageArray[2]));
-    }
-
-    public String toString(){
-        return messageArray[0] + "," + messageArray[1] + "," + messageArray[2] + ";";
-    }
-
+   /**
+    * Returns a String representation of the TextMessage.
+    *
+    * @return a formatted string containing sender, receiver, and message content
+    */
+   @Override
+   public String toString() {
+       return messageArray[0] + "," + messageArray[1] + "," + messageArray[2] + ";";
+   }
 }
