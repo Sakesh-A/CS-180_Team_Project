8e81e2b7cada302d24c254db1782abe5c8d17ed1
Sakesh-A <a.sakesh@gmail.com>
2024-11-03 16:42:27 -0500

    changes to main that still need to be updated

--- database/Main.java
+++ database/Main.java
@@ -1,46 +1,105 @@
-//import java.util.*;
-//import java.io.*;
-//
-//
-//public class Main {
-//    public static void main(String[] args) {
-//        UserDatabase db = new UserDatabase();
-//        Scanner sc = new Scanner(System.in);
-//
-//        System.out.println("enter your username:");
-//        String user = sc.nextLine();
-//
-//        System.out.println("Enter your password:");
-//        String pass = sc.nextLine();
-//
-//        while (!db.isPasswordValid(pass)) {
-//            System.out.println("invalid, try again");
-//            pass = sc.nextLine();
-//        }
-//
-//        System.out.println("would you like to recieve messages from anybody?");
-//        String choice = sc.nextLine();
-//
-//        boolean yOrN = false;
-//
-//        if (choice.equals("y")) {
-//            yOrN = true;
-//        } else {
-//            yOrN = false;
-//        }
-//
-//        if(db.saveUser(user, pass, yOrN)) {
-//            String filePath = "UsersList.txt";
-//            try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-//                String line;
-//                while ((line = br.readLine()) != null) {
-//                    System.out.println(line);
-//                }
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//
-//
-//    }
-//}
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        try (BufferedReader inputReader = new BufferedReader(new FileReader("input.txt"))) {
+            String[] files = inputReader.readLine().split(" ");
+            if (files.length < 4) {
+                System.out.println("Insufficient file names in input.txt");
+                return;
+            }
+
+            String scpIn = files[0];               // SCP input file
+            String researcherIn = files[1];       // Researcher input file
+            String databaseOutput = files[2];     // Database output file
+            String mainOut = files[3];            // Main output file
+
+            FoundationDatabase db = new FoundationDatabase(scpIn, researcherIn, databaseOutput);
+
+            try (BufferedWriter mainOutWriter = new BufferedWriter(new FileWriter(mainOut))) {
+                mainOutWriter.write("Foundation Database Started");
+                mainOutWriter.newLine();
+
+                String commandLine;
+
+                while ((commandLine = inputReader.readLine()) != null) {
+                    boolean result = false;
+
+                    try {
+                        int command = Integer.parseInt(commandLine);
+                        switch (command) {
+                            case 1:
+                                result = db.readSCP();
+                                break;
+                            case 2:
+                                result = db.readResearcher();
+                                break;
+                            case 3:
+                                result = db.autoAssignResearcher();
+                                break;
+                            case 4:
+                                String one = inputReader.readLine();
+                                String two = inputReader.readLine();
+                                if (one != null && two != null) {
+                                    result = db.modifySCP(one, two);
+                                } else {
+                                    result = false;
+                                }
+                                break;
+                            case 5:
+                                String three = inputReader.readLine();
+                                String four = inputReader.readLine();
+                                if (three != null && four != null) {
+                                    result = db.modifyResearcher(three, four);
+                                } else {
+                                    result = false;
+                                }
+                                break;
+                            case 6:
+                                String five = inputReader.readLine();
+                                String six = inputReader.readLine();
+                                try {
+                                    int seven = Integer.parseInt(five);
+                                    if (six != null) {
+                                        result = db.addResearcher(seven, six);
+                                    } else {
+                                        result = false;
+                                    }
+                                } catch (NumberFormatException e) {
+                                    result = false;
+                                }
+                                break;
+                            case 7:
+                                result = db.outputDatabase();
+                                break;
+                            default:
+                                // nothign to do
+                        }
+
+                        if (result) {
+                            mainOutWriter.write(command + " Success");
+                        } else {
+                            mainOutWriter.write(command + " Failure");
+                        }
+                        mainOutWriter.newLine();
+
+                    } catch (NumberFormatException e) {
+                        mainOutWriter.write("Command Failure");
+                        mainOutWriter.newLine();
+                    } catch (IOException e) {
+                        mainOutWriter.write("IO Read Failure");
+                        mainOutWriter.newLine();
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("IO Read Failure");
+            }
+        } catch (IOException e) {
+            System.out.println("IO Read Failure");
+        }
+    }
+
+    }
+}
