5aa2fdcd2f9393a77dec8960472f3be8610e818d
danielkzhang123 <zhan4728@purdue.edu>
2024-11-03 21:33:53 -0500

    Update UserDatabase.java

--- database/UserDatabase.java
+++ database/UserDatabase.java
@@ -13,12 +13,11 @@ import java.io.*;
  *
  */
 
-public class UserDatabase extends Thread implements UserDatabaseInterface {
+public class UserDatabase implements UserDatabaseInterface {
     // Fields
     private ArrayList<User> users;
     private ArrayList<String> userFiles;
     private ArrayList<BufferedWriter> writers;
-    Object obj = new Object();
 
     // Constructor
     public UserDatabase() {
@@ -39,72 +38,47 @@ public class UserDatabase extends Thread implements UserDatabaseInterface {
 
     // addUser method
     public boolean addUser(User user) {
-        synchronized(obj) {
-            for (User existingUser : users) {
-                if (existingUser.getUsername().equals(user.getUsername())) {
-                    System.err.println("User with this username already exists.");
-                    return false;
-                }
-            }
-            users.add(user);
-            String file = String.format("%s.txt", user.getUsername());
-            userFiles.add(file);
-            try {
-                writers.add(new BufferedWriter(new FileWriter(file)));
-            } catch(IOException e) {
-
+        for (User existingUser : users) {
+            if (existingUser.getUsername().equals(user.getUsername())) {
+                System.err.println("User with this username already exists.");
                 return false;
             }
-            return true;
         }
-<<<<<<< HEAD
         users.add(user);
         String file = String.format("%s.txt", user.getUsername());
         userFiles.add(file);
         try {
-            BufferedWriter br = new BufferedWriter(new FileWriter(file));
-            br.close();
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+            bw.close();
         } catch(IOException e) {
 
             return false;
         }
         return true;
-=======
->>>>>>> ab5adf8a4e657ea0e1e3cbbabbc17f15896a8d77
     }
 
     // removeUser method
     public boolean removeUser(User user) {
-        synchronized(obj) {
-            users.remove(user);
-            for (int i = 0; i < users.size(); i++) {
-                if (users.get(i).hasFriended(user)) {
-                    users.get(i).removeFriend(user);
-                }
+        users.remove(user);
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).hasFriended(user)) {
+                users.get(i).removeFriend(user);
+                break;
             }
-<<<<<<< HEAD
         }
         for(int i = 0; i < userFiles.size(); i++) {
             String temp = userFiles.get(i).substring(0, userFiles.get(i).lastIndexOf("."));
             if(user.getUsername().equals(temp)) {
                 userFiles.remove(userFiles.get(i));
                 //writers.remove(i);
-=======
-            for(int i = 0; i < userFiles.size(); i++) {
-                String temp = userFiles.get(i).substring(0, userFiles.get(i).lastIndexOf("."));
-                if(user.getUsername().equals(temp)) {
-                    userFiles.remove(userFiles.get(i));
-                    writers.remove(i);
-                }
->>>>>>> ab5adf8a4e657ea0e1e3cbbabbc17f15896a8d77
+                break;
             }
-            return true;
-        }       
+        }
+        return true;
     }
 
     // saveUser method
     public boolean everythingToFile() {
-<<<<<<< HEAD
         String filename = "UsersList.txt";
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename, false))) {
             for (User user : users) {
@@ -119,35 +93,16 @@ public class UserDatabase extends Thread implements UserDatabaseInterface {
             User user = users.get(i);
             String name = user.getUsername() + ".txt";
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(name, false))) {
+
                 ArrayList<TextMessage> messages = user.getMessages();
                 for (TextMessage message : messages) {
                     writer.write(message.toString());
-=======
-        synchronized(obj) {
-            String filename = "UsersList.txt";
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename, false))) {
-                for (User user : users) {
-                    writer.write(user.toString() + "\n");
->>>>>>> ab5adf8a4e657ea0e1e3cbbabbc17f15896a8d77
                 }
-            } catch (IOException e) {
+            } catch(IOException e) {
                 System.err.println("Error writing users to file: " + e.getMessage());
                 return false;
             }
-
-            for (int i = 0; i < users.size(); i++) {
-                try (BufferedWriter writer = writers.get(i)) {
-                    User user = users.get(i);
-                    ArrayList<TextMessage> messages = user.getMessages();
-                    for (TextMessage message : messages) {
-                        writer.write(message.toString());
-                    }
-                } catch(IOException e) {
-                    System.err.println("Error writing users to file: " + e.getMessage());
-                    return false;
-                }
-            }
-            return true;
-        }     
+        }
+        return true;
     }
 }
