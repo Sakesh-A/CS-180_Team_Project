aa53e56ace01b716ab2c33f5919900a6af426c76
Sakesh-A <a.sakesh@gmail.com>
2024-11-14 18:49:11 -0500

    changes to client and server

--- Server/Client.java
+++ Server/Client.java
@@ -2,3 +2,57 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+
+public class Client {
+    public static void main(String[] args) {
+        Socket socket = null;
+        InputStreamReader in = null;
+        OutputStreamWriter out = null;
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+
+        try {
+            socket = new Socket("localhost", 1234);
+
+            in = new InputStreamReader(socket.getInputStream);
+            out = new OutputStreamWriter(socket.getOutputStream);
+
+            br = new BufferedReader(in);
+            bw = new BufferedWriter(out);
+
+            Scanner scan = new Scanner(System.in);
+
+            while (true) {
+                String existing = scanner.nextLine();
+                bw.write(existing);
+                bw.newLine();
+                bw.flush();
+
+                System.out.println("Server" + br.readLine());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (socket != null) {
+                    socket.close();
+                }
+                if (in != null) {
+                    in.close();
+                }
+                if (out != null) {
+                    out.close();
+                }
+                if (br != null) {
+                    br.close();
+                }
+                if (bw != null) {
+                    bw.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
--- Server/Server.java
+++ Server/Server.java
@@ -2,3 +2,17 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+
+public class Server {
+    public static void main(String[] args) {
+        Socket socket = null;
+        InputStreamReader in = null;
+        BufferedReader br = null;
+        OutputStreamWriter out = null;
+        BufferedWriter bw = null;
+        ServerSocket serverSocket = null;
+
+
+
+    }
+}
