6551578c113b67ae7b5ee02892dcff14f31567cc
Sakesh-A <a.sakesh@gmail.com>
2024-11-14 19:07:47 -0500

    readded server

---
+++ Server/Server.java
@@ -0,0 +1,26 @@
+import java.net.*;
+import java.io.*;
+
+public class Server
+{
+    public static void main(String[] args) throws IOException
+    {
+        ServerSocket serverSocket = null;
+        boolean happening = true;
+
+        try
+        {
+            serverSocket = new ServerSocket(4444);
+        }
+        catch (IOException e)
+        {
+            System.err.println("Could not listen on port: 4444.");
+            System.exit(-1);
+        }
+
+        while (happening)
+            new Thread (new ClientHandler (serverSocket.accept())).start();
+
+        serverSocket.close();
+    }
+}
