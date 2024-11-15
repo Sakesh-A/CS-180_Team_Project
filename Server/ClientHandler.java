500749f5c85925801c6d5c7f136bf4ee377eff7e
Zoconne <zoconne@purdue.edu>
2024-11-14 19:29:28 -0500

    Updated ClientHandler

--- Server/ClientHandler.java
+++ Server/ClientHandler.java
@@ -1,53 +1,77 @@
+
 import java.net.*;
 import java.io.*;
+import java.util.*;
+;
 
 class ClientHandler implements Runnable
 {
     private Socket client;
+    Scanner sc = new Scanner(System.in);
+    String choice = "";
+    boolean isPublic = true;
 
-    public ClientHandler (Socket c)
-    {
+    public ClientHandler (Socket c) {
         client = c;
     }
 
-    public void run ()
-    {
+    public void run () {
         BufferedReader clientIn = null;
         PrintWriter clientOut = null;
         String input = null;
 
-        try
-        {
+        try {
             clientIn = new BufferedReader (new InputStreamReader (client.getInputStream ()));
             clientOut = new PrintWriter (client.getOutputStream (), true);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace ();
-        }
+        } 
+        
+        while(true) {
+            System.out.println("Do you have an account?");
+            choice = sc.nextLine();
 
-        do
-        {
-            try
-            {
+            if (choice.equalsIgnoreCase("yes")) {
+                System.out.println("Enter in your Username:");
+                String user = sc.nextLine();
+
+                System.out.println("Enter in your Password:");
+                String pass = sc.nextLine();
+
+            } else if (choice.equalsIgnoreCase("no")) {
+                System.out.println("Please enter a username:");
+                String user = sc.nextLine();
+
+                System.out.println("Please enter a password:");
+                String pass = sc.nextLine();
+
+                System.out.println("Would you like anybody to be able to view your profile?");
+                choice = sc.nextLine();
+                if (choice.equalsIgnoreCase("yes")) { 
+                    boolean isPublic = true;
+                } else if (choice.equalsIgnoreCase("no"))  {
+                    boolean isPublic = false;
+                }
+
+
+
+            }
+
+            try {
                 input = clientIn.readLine ();
                 clientOut.println (input);
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 e.printStackTrace ();
             }
-        }
-        while (! input.equals ("Bye"));
 
-        try
-        {
+            break;
+        } 
+
+        try {
             clientIn.close ();
             clientOut.close ();
             client.close ();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace ();
         }
     }
